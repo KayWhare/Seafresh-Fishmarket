ac457d2f59fa6717a2b77e31ec80b067405ad735
kayWhare <k_whare@hotmail.com>
2020-04-12 18:10:34 +0800

    Merged initial projects into a single core template(swiper, navbar and sidemenu)

---
+++ script.js
@@ -0,0 +1,69 @@
+
+// -----------------------NAV COLOR CHANGE -----------------------
+
+window.addEventListener("scroll", function (event) {
+   let headerDiv = document.getElementById('nav');
+   let spinner = document.querySelectorAll('.spinner');
+
+
+   if (window.pageYOffset > 0){
+      headerDiv.style.backgroundColor="rgba(0, 0, 0, 0.3)";
+      headerDiv.children[0].style.color = 'white';
+      headerDiv.children[2].style.color = 'white';
+      headerDiv.children[3].style.color = 'white';
+      for (i=0; i < spinner.length; i++){
+         spinner[i].style.backgroundColor='white'
+      };
+   }else{
+      headerDiv.style.backgroundColor="rgba(255, 255, 255, 0.6)";
+      headerDiv.children[0].style.color = 'black';
+      headerDiv.children[2].style.color = 'black';
+      headerDiv.children[3].style.color = 'black';
+      for (i=0; i < spinner.length; i++){
+         spinner[i].style.backgroundColor='black'
+      };
+   }
+});
+
+
+// ----------------------TOGGLE FOR SIDEMENU-------------------
+
+var checkbox = document.getElementById('openSidebarMenu');
+
+checkbox.addEventListener('click', function(){
+   let sidebarMenu = document.getElementById('sidebarMenu');
+
+   if (checkbox.checked == true){
+      return sidebarMenu.style.transform = 'translateX(0)';
+   }else{
+      sidebarMenu.style.transform = 'translateX(-250px)';
+   }
+});
+
+//-----------------------Searchbar playaround------------------
+
+let searchbtn = document.getElementById('searchIcon');
+let searchbar = document.getElementsByClassName('search-box')[0];
+
+searchbtn.addEventListener('click', ()=>{
+   if (searchbar.classList.contains('active')){
+      searchbar.classList.remove('active');
+   }else{
+      searchbar.classList.add('active');
+   }
+}
+);
+
+// -----------------------SWIPER-------------------------------
+
+var swiper = new Swiper('.swiper-container', {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   slidesPerGroup: 3,
+   loop: true,
+   loopFillGroupWithBlank: true,
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   },
+ });
